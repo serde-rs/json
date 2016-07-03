06536eb4557319386c16d1b05941ea34fd4e7d09
David Tolnay <dtolnay@gmail.com>
2016-07-03 09:47:33 -0700

    Rename Object/ArrayBuilder::unwrap() to build()

--- json_tests/tests/test_json_builder.rs
+++ json_tests/tests/test_json_builder.rs
@@ -4,19 +4,19 @@ use serde_json::builder::{ArrayBuilder, ObjectBuilder};
 
 #[test]
 fn test_array_builder() {
-    let value = ArrayBuilder::new().unwrap();
+    let value = ArrayBuilder::new().build();
     assert_eq!(value, Value::Array(Vec::new()));
 
     let value = ArrayBuilder::new()
         .push(1)
         .push(2)
         .push(3)
-        .unwrap();
+        .build();
     assert_eq!(value, Value::Array(vec!(Value::U64(1), Value::U64(2), Value::U64(3))));
 
     let value = ArrayBuilder::new()
         .push_array(|bld| bld.push(1).push(2).push(3))
-        .unwrap();
+        .build();
     assert_eq!(value, Value::Array(vec!(Value::Array(vec!(Value::U64(1), Value::U64(2), Value::U64(3))))));
 
     let value = ArrayBuilder::new()
@@ -24,7 +24,7 @@ fn test_array_builder() {
             bld
                 .insert("a".to_string(), 1)
                 .insert("b".to_string(), 2))
-        .unwrap();
+        .build();
 
     let mut map = Map::new();
     map.insert("a".to_string(), Value::U64(1));
@@ -34,13 +34,13 @@ fn test_array_builder() {
 
 #[test]
 fn test_object_builder() {
-    let value = ObjectBuilder::new().unwrap();
+    let value = ObjectBuilder::new().build();
     assert_eq!(value, Value::Object(Map::new()));
 
     let value = ObjectBuilder::new()
         .insert("a".to_string(), 1)
         .insert("b".to_string(), 2)
-        .unwrap();
+        .build();
 
     let mut map = Map::new();
     map.insert("a".to_string(), Value::U64(1));
