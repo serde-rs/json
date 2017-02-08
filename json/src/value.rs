df0a42e764a7e13537719dfad8e0920c83a791fd
David Tolnay <dtolnay@gmail.com>
2017-02-07 19:30:04 -0500

    IndexMut for Map and Value

--- json/src/value.rs
+++ json/src/value.rs
@@ -499,10 +499,20 @@ impl Default for Value {
 /// This trait is sealed and cannot be implemented for types outside of
 /// `serde_json`.
 pub trait Index: private::Sealed {
+    /// Return None if the key is not already in the array or object.
     #[doc(hidden)]
     fn index_into<'v>(&self, v: &'v Value) -> Option<&'v Value>;
+
+    /// Return None if the key is not already in the array or object.
     #[doc(hidden)]
     fn index_into_mut<'v>(&self, v: &'v mut Value) -> Option<&'v mut Value>;
+
+    /// Panic if array index out of bounds. If key is not already in the object,
+    /// insert it with a value of null. Panic if Value is a type that cannot be
+    /// indexed into, except if Value is null then it can be treated as an empty
+    /// object.
+    #[doc(hidden)]
+    fn index_or_insert<'v>(&self, v: &'v mut Value) -> &'v mut Value;
 }
 
 impl Index for usize {
@@ -516,6 +526,18 @@ impl Index for usize {
         match *v {
             Value::Array(ref mut vec) => vec.get_mut(*self),
             _ => None,
+        }
+    }
+    fn index_or_insert<'v>(&self, v: &'v mut Value) -> &'v mut Value {
+        match *v {
+            Value::Array(ref mut vec) => {
+                let len = vec.len();
+                vec.get_mut(*self).unwrap_or_else(|| {
+                    panic!("cannot access index {} of JSON array of length {}",
+                           self, len)
+                })
+            }
+            _ => panic!("cannot access index {} of JSON {}", self, Type(v)),
         }
     }
 }
@@ -533,20 +555,35 @@ impl Index for str {
             _ => None,
         }
     }
+    fn index_or_insert<'v>(&self, v: &'v mut Value) -> &'v mut Value {
+        if let Value::Null = *v {
+            let mut map = Map::new();
+            map.insert(self.to_owned(), Value::Null);
+            *v = Value::Object(map);
+        }
+        match *v {
+            Value::Object(ref mut map) => {
+                // TODO: use entry() once LinkedHashMap supports entry()
+                // https://github.com/contain-rs/linked-hash-map/issues/5
+                if !map.contains_key(self) {
+                    map.insert(self.to_owned(), Value::Null);
+                }
+                map.get_mut(self).unwrap()
+            }
+            _ => panic!("cannot access key {:?} in JSON {}", self, Type(v)),
+        }
+    }
 }
 
 impl Index for String {
     fn index_into<'v>(&self, v: &'v Value) -> Option<&'v Value> {
-        match *v {
-            Value::Object(ref map) => map.get(self),
-            _ => None,
-        }
+        self[..].index_into(v)
     }
     fn index_into_mut<'v>(&self, v: &'v mut Value) -> Option<&'v mut Value> {
-        match *v {
-            Value::Object(ref mut map) => map.get_mut(self),
-            _ => None,
-        }
+        self[..].index_into_mut(v)
+    }
+    fn index_or_insert<'v>(&self, v: &'v mut Value) -> &'v mut Value {
+        self[..].index_or_insert(v)
     }
 }
 
@@ -557,6 +594,9 @@ impl<'a, T: ?Sized> Index for &'a T where T: Index {
     fn index_into_mut<'v>(&self, v: &'v mut Value) -> Option<&'v mut Value> {
         (**self).index_into_mut(v)
     }
+    fn index_or_insert<'v>(&self, v: &'v mut Value) -> &'v mut Value {
+        (**self).index_or_insert(v)
+    }
 }
 
 // Prevent users from implementing the Index trait.
@@ -566,6 +606,22 @@ mod private {
     impl Sealed for str {}
     impl Sealed for String {}
     impl<'a, T: ?Sized> Sealed for &'a T where T: Sealed {}
+}
+
+/// Used in panic messages.
+struct Type<'a>(&'a Value);
+
+impl<'a> fmt::Display for Type<'a> {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        match *self.0 {
+            Value::Null => formatter.write_str("null"),
+            Value::Bool(_) => formatter.write_str("boolean"),
+            Value::Number(_) => formatter.write_str("number"),
+            Value::String(_) => formatter.write_str("string"),
+            Value::Array(_) => formatter.write_str("array"),
+            Value::Object(_) => formatter.write_str("object"),
+        }
+    }
 }
 
 // The usual semantics of Index is to panic on invalid indexing.
@@ -622,6 +678,46 @@ impl<I> ops::Index<I> for Value where I: Index {
     fn index(&self, index: I) -> &Value {
         static NULL: Value = Value::Null;
         index.index_into(self).unwrap_or(&NULL)
+    }
+}
+
+impl<I> ops::IndexMut<I> for Value where I: Index {
+    /// Write into a `serde_json::Value` using the syntax `value[0] = ...` or
+    /// `value["k"] = ...`.
+    ///
+    /// If the index is a number, the value must be an array of length bigger
+    /// than the index. Indexing into a value that is not an array or an array
+    /// that is too small will panic.
+    ///
+    /// If the index is a string, the value must be an object or null which is
+    /// treated like an empty object. If the key is not already present in the
+    /// object, it will be inserted with a value of null. Indexing into a value
+    /// that is neither an object nor null will panic.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # #[macro_use] extern crate serde_json;
+    /// # fn main() {
+    /// let mut data = json!({ "x": 0 });
+    ///
+    /// // replace an existing key
+    /// data["x"] = json!(1);
+    ///
+    /// // insert a new key
+    /// data["y"] = json!([false, false, false]);
+    ///
+    /// // replace an array value
+    /// data["y"][0] = json!(true);
+    ///
+    /// // inserted a deeply nested key
+    /// data["a"]["b"]["c"]["d"] = json!(true);
+    ///
+    /// println!("{}", data);
+    /// # }
+    /// ```
+    fn index_mut(&mut self, index: I) -> &mut Value {
+        index.index_or_insert(self)
     }
 }
 
