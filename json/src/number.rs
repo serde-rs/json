4f5bfbc6c109c7d26a2d64fe89441d02b00a33bd
David Tolnay <dtolnay@gmail.com>
2017-02-11 11:56:40 -0800

    Restore non arbitrary precision implementation of Number

--- json/src/number.rs
+++ json/src/number.rs
@@ -1,18 +1,67 @@
-use dtoa;
 use error::Error;
-use itoa;
 use serde::de::{self, Visitor};
 use serde::{Serialize, Serializer, Deserialize, Deserializer};
 use std::fmt::{self, Display};
 use std::i64;
 
+#[cfg(not(feature = "arbitrary_precision"))]
+use num_traits::NumCast;
+#[cfg(not(feature = "arbitrary_precision"))]
+use std::fmt::Debug;
+
 #[cfg(feature = "arbitrary_precision")]
 use std::io;
+#[cfg(feature = "arbitrary_precision")]
+use dtoa;
+#[cfg(feature = "arbitrary_precision")]
+use itoa;
 
 /// Represents a JSON number, whether integer or floating point.
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, PartialEq)]
+#[cfg_attr(feature = "arbitrary_precision", derive(Debug))]
 pub struct Number {
-    n: String,
+    n: N,
+}
+
+#[cfg(not(feature = "arbitrary_precision"))]
+// "N" is a prefix of "NegInt"... this is a false positive.
+// https://github.com/Manishearth/rust-clippy/issues/1241
+#[cfg_attr(feature = "cargo-clippy", allow(enum_variant_names))]
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum N {
+    PosInt(u64),
+    /// Always less than zero.
+    NegInt(i64),
+    /// Always finite.
+    Float(f64),
+}
+
+#[cfg(feature = "arbitrary_precision")]
+type N = String;
+
+macro_rules! cast_methods {
+    ($(
+        #[doc = $doc:tt]
+        pub fn $method:ident(&self) -> Option<$ty:ident>;
+    )+) => {
+        $(
+            #[cfg(not(feature = "arbitrary_precision"))]
+            #[doc = $doc]
+            pub fn $method(&self) -> Option<$ty> {
+                match self.n {
+                    N::PosInt(n) => NumCast::from(n),
+                    N::NegInt(n) => NumCast::from(n),
+                    N::Float(n) => NumCast::from(n),
+                }
+            }
+
+            #[cfg(feature = "arbitrary_precision")]
+            #[doc = $doc]
+            pub fn $method(&self) -> Option<$ty> {
+                self.n.parse().ok()
+            }
+        )*
+    };
 }
 
 impl Number {
@@ -34,22 +83,15 @@ impl Number {
         self.as_f64().is_some()
     }
 
-    /// Returns the number represented as i64 if possible, or else None.
-    #[inline]
-    pub fn as_i64(&self) -> Option<i64> {
-        self.n.parse().ok()
-    }
+    cast_methods! {
+        /// Returns the number represented as i64 if possible, or else None.
+        pub fn as_i64(&self) -> Option<i64>;
 
-    /// Returns the number represented as u64 if possible, or else None.
-    #[inline]
-    pub fn as_u64(&self) -> Option<u64> {
-        self.n.parse().ok()
-    }
+        /// Returns the number represented as u64 if possible, or else None.
+        pub fn as_u64(&self) -> Option<u64>;
 
-    /// Returns the number represented as f64 if possible, or else None.
-    #[inline]
-    pub fn as_f64(&self) -> Option<f64> {
-        self.n.parse().ok()
+        /// Returns the number represented as f64 if possible, or else None.
+        pub fn as_f64(&self) -> Option<f64>;
     }
 
     /// Converts a finite f64 to a Number. Infinite or NaN values are not JSON
@@ -57,15 +99,14 @@ impl Number {
     #[inline]
     pub fn from_f64(f: f64) -> Option<Number> {
         if f.is_finite() {
-            let mut buf = Vec::new();
-            dtoa::write(&mut buf, f).unwrap();
-            Some(Number { n: String::from_utf8(buf).unwrap() })
+            Some(Number { n: n_from_finite_f64(f) })
         } else {
             None
         }
     }
 
     /// Not public API. Should be pub(crate). The deserializer uses this.
+    #[cfg(feature = "arbitrary_precision")]
     #[doc(hidden)]
     #[inline]
     pub fn from_string_unchecked(n: String) -> Self {
@@ -73,9 +114,38 @@ impl Number {
     }
 }
 
+#[cfg(not(feature = "arbitrary_precision"))]
+fn n_from_finite_f64(f: f64) -> N {
+    N::Float(f)
+}
+
+#[cfg(feature = "arbitrary_precision")]
+fn n_from_finite_f64(f: f64) -> N {
+    let mut buf = Vec::new();
+    dtoa::write(&mut buf, f).unwrap();
+    String::from_utf8(buf).unwrap()
+}
+
 impl Display for Number {
+    #[cfg(not(feature = "arbitrary_precision"))]
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        match self.n {
+            N::PosInt(u) => Display::fmt(&u, formatter),
+            N::NegInt(i) => Display::fmt(&i, formatter),
+            N::Float(f) => Display::fmt(&f, formatter),
+        }
+    }
+
+    #[cfg(feature = "arbitrary_precision")]
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         formatter.write_str(&self.n)
+    }
+}
+
+#[cfg(not(feature = "arbitrary_precision"))]
+impl Debug for Number {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        Debug::fmt(&self.n, formatter)
     }
 }
 
@@ -85,7 +155,11 @@ impl Serialize for Number {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
         where S: Serializer
     {
-        serialize_number(serializer, self)
+        match self.n {
+            N::PosInt(i) => serializer.serialize_u64(i),
+            N::NegInt(i) => serializer.serialize_i64(i),
+            N::Float(f) => serializer.serialize_f64(f),
+        }
     }
 
     #[cfg(feature = "arbitrary_precision")]
@@ -100,7 +174,15 @@ impl Serialize for Number {
         impl<T> SerializeNumber for T where T: Serializer {
             #[inline]
             default fn serialize_number(self, number: &Number) -> Result<T::Ok, T::Error> {
-                serialize_number(self, number)
+                if let Some(u) = number.as_u64() {
+                    self.serialize_u64(u)
+                } else if let Some(i) = number.as_i64() {
+                    self.serialize_i64(i)
+                } else if let Some(f) = number.as_f64() {
+                    self.serialize_f64(f)
+                } else {
+                    self.serialize_str(&number.n)
+                }
             }
         }
 
@@ -115,20 +197,6 @@ impl Serialize for Number {
         }
 
         serializer.serialize_number(self)
-    }
-}
-
-fn serialize_number<S>(serializer: S, number: &Number) -> Result<S::Ok, S::Error>
-    where S: Serializer
-{
-    if let Some(u) = number.as_u64() {
-        serializer.serialize_u64(u)
-    } else if let Some(i) = number.as_i64() {
-        serializer.serialize_i64(i)
-    } else if let Some(f) = number.as_f64() {
-        serializer.serialize_f64(f)
-    } else {
-        serializer.serialize_str(&number.n)
     }
 }
 
@@ -174,6 +242,19 @@ impl Deserialize for Number {
 impl Deserializer for Number {
     type Error = Error;
 
+    #[cfg(not(feature = "arbitrary_precision"))]
+    #[inline]
+    fn deserialize<V>(self, visitor: V) -> Result<V::Value, Error>
+        where V: Visitor
+    {
+        match self.n {
+            N::PosInt(u) => visitor.visit_u64(u),
+            N::NegInt(i) => visitor.visit_i64(i),
+            N::Float(f) => visitor.visit_f64(f),
+        }
+    }
+
+    #[cfg(feature = "arbitrary_precision")]
     #[inline]
     fn deserialize<V>(self, visitor: V) -> Result<V::Value, Error>
         where V: Visitor
@@ -196,6 +277,44 @@ impl Deserializer for Number {
     }
 }
 
+#[cfg(not(feature = "arbitrary_precision"))]
+macro_rules! from_signed {
+    ($($signed_ty:ident)*) => {
+        $(
+            impl From<$signed_ty> for Number {
+                #[inline]
+                fn from(i: $signed_ty) -> Self {
+                    if i < 0 {
+                        Number { n: N::NegInt(i as i64) }
+                    } else {
+                        Number { n: N::PosInt(i as u64) }
+                    }
+                }
+            }
+        )*
+    };
+}
+
+#[cfg(not(feature = "arbitrary_precision"))]
+macro_rules! from_unsigned {
+    ($($unsigned_ty:ident)*) => {
+        $(
+            impl From<$unsigned_ty> for Number {
+                #[inline]
+                fn from(u: $unsigned_ty) -> Self {
+                    Number { n: N::PosInt(u as u64) }
+                }
+            }
+        )*
+    };
+}
+
+#[cfg(not(feature = "arbitrary_precision"))]
+from_signed!(i8 i16 i32 i64 isize);
+#[cfg(not(feature = "arbitrary_precision"))]
+from_unsigned!(u8 u16 u32 u64 usize);
+
+#[cfg(feature = "arbitrary_precision")]
 macro_rules! from_primitive {
     ($($ty:ident)*) => {
         $(
@@ -211,4 +330,5 @@ macro_rules! from_primitive {
     };
 }
 
+#[cfg(feature = "arbitrary_precision")]
 from_primitive!(i8 i16 i32 i64 isize u8 u16 u32 u64 usize);
