serde-rs/json

commit 8123ecc474ea599d4659f20c3cfbfb7011b77628

Fix #256: support deserializing strings with arbitrary bytes

json/src/de.rs
@@ -649,9 +649,72 @@ impl<'a, R: Read> de::Deserializer for &'a mut Deserializer<R> {
         }
     }
 
+    /// Parses a JSON string as bytes. Note that this function does not
+    /// check whether the bytes represent valid unicode code points.
+    ///
+    /// The JSON specification requires that strings only contain valid
+    /// unicode characters. To deal with non-conforming JSON, you may use
+    /// this function, which attempts to parse a string without checking
+    /// whether the bytes represent valid unicode code points.
+    /// number of extra processing as possible.
+    ///
+    /// Escape sequences are processed as usual, and for `\uXXXX` escapes 
+    /// it is still checked if the hex number represents a valid unicode
+    /// code point.
+    ///
+    /// # Example usage
+    ///
+    /// You can use this to parse JSON strings containing non-unicode bytes:
+    ///
+    /// ```
+    /// # extern crate serde;
+    /// # extern crate serde_json;
+    /// #
+    /// let bytes = serde::bytes::ByteBuf::from(b"some raw bytes: \xe5\x00\xe5".to_vec());
+    /// let parsed = serde_json::from_slice(  b"\"some raw bytes: \xe5\x00\xe5\"").unwrap();
+    /// 
+    /// assert_eq!(bytes, parsed);
+    /// ```
+    ///
+    /// `\u` escape sequences with invalid unicode code points still fail to parse:
+    ///
+    /// ```
+    /// # extern crate serde;
+    /// # extern crate serde_json;
+    /// #
+    /// let json = "\"invalid unicode surrogate: \\uD801\"";
+    /// let parsed: Result<serde::bytes::ByteBuf, _> = serde_json::from_str(json);
+    /// assert!(parsed.is_err(), "{} should not parse: {:?}", json, parsed);
+    /// ```
+    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value>
+        where V: de::Visitor
+    {
+        try!(self.parse_whitespace());
+
+        let next = try!(self.peek());
+        let next = try!(next.ok_or(self.peek_error(ErrorCode::ExpectedSomeString)));
+
+        match next {
+            b'"' => {
+                self.eat_char();
+                let slice = try!(self.read.parse_str_raw(&mut self.str_buf));
+                visitor.visit_bytes(slice)
+            }
+            _ => self.deserialize(visitor),
+        }
+
+    }
+
+    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value>
+        where V: de::Visitor
+    {
+        self.deserialize_bytes(visitor)
+    }
+
+
     forward_to_deserialize! {
         bool u8 u16 u32 u64 i8 i16 i32 i64 f32 f64 char str string unit seq
-        seq_fixed_size bytes byte_buf map unit_struct tuple_struct struct
+        seq_fixed_size map unit_struct tuple_struct struct
         struct_field tuple ignored_any
     }
 }

json/src/error.rs
@@ -59,6 +59,9 @@ pub enum ErrorCode {
     /// Expected this character to start a JSON value.
     ExpectedSomeValue,
 
+    /// Expected this character to start a JSON string.
+    ExpectedSomeString,
+
     /// Invalid hex escape code.
     InvalidEscape,
 
@@ -139,6 +142,9 @@ impl Display for ErrorCode {
             }
             ErrorCode::ExpectedSomeValue => {
                 f.write_str("expected value")
+            }
+            ErrorCode::ExpectedSomeString => {
+                f.write_str("expected string")
             }
             ErrorCode::InvalidEscape => {
                 f.write_str("invalid escape")

json/src/read.rs
@@ -46,8 +46,20 @@ pub trait Read: private::Sealed {
     #[doc(hidden)]
     fn parse_str<'s>(
         &'s mut self,
-        scratch: &'s mut Vec<u8>
+        scratch: &'s mut Vec<u8>,
     ) -> Result<&'s str>;
+
+    /// Assumes the previous byte was a quotation mark. Parses a JSON-escaped
+    /// string until the next quotation mark using the given scratch space if
+    /// necessary. The scratch space is initially empty.
+    ///
+    /// This function returns the raw bytes in the string with escape sequences
+    /// expanded but without performing unicode validation.
+    #[doc(hidden)]
+    fn parse_str_raw<'s>(
+        &'s mut self,
+        scratch: &'s mut Vec<u8>
+    ) -> Result<&'s [u8]>;
 }
 
 pub struct Position {
@@ -97,6 +109,47 @@ impl<Iter> IteratorRead<Iter>
 
 impl<Iter> private::Sealed for IteratorRead<Iter>
     where Iter: Iterator<Item = io::Result<u8>> {}
+
+impl<Iter> IteratorRead<Iter>
+    where Iter: Iterator<Item = io::Result<u8>>
+{
+    fn parse_str_bytes<'s, T, F>(
+        &'s mut self,
+        scratch: &'s mut Vec<u8>,
+        validate: bool,
+        result: F
+    ) -> Result<T>
+        where T: 's,
+              F: FnOnce(&'s Self, &'s [u8]) -> Result<T>,
+    {
+        loop {
+            let ch = match try!(self.next()) {
+                Some(ch) => ch,
+                None => {
+                    return error(self, ErrorCode::EOFWhileParsingString);
+                }
+            };
+            if !ESCAPE[ch as usize] {
+                scratch.push(ch);
+                continue;
+            }
+            match ch {
+                b'"' => {
+                    return result(self, scratch);
+                }
+                b'\\' => {
+                    try!(parse_escape(self, scratch));
+                }
+                _ => {
+                    if validate {
+                        return error(self, ErrorCode::InvalidUnicodeCodePoint);
+                    }
+                    scratch.push(ch);
+                }
+            }
+        }
+    }
+}
 
 impl<Iter> Read for IteratorRead<Iter>
     where Iter: Iterator<Item = io::Result<u8>>,
@@ -154,29 +207,14 @@ impl<Iter> Read for IteratorRead<Iter>
         &'s mut self,
         scratch: &'s mut Vec<u8>
     ) -> Result<&'s str> {
-        loop {
-            let ch = match try!(self.next()) {
-                Some(ch) => ch,
-                None => {
-                    return error(self, ErrorCode::EOFWhileParsingString);
-                }
-            };
-            if !ESCAPE[ch as usize] {
-                scratch.push(ch);
-                continue;
-            }
-            match ch {
-                b'"' => {
-                    return as_str(self, scratch);
-                }
-                b'\\' => {
-                    try!(parse_escape(self, scratch));
-                }
-                _ => {
-                    return error(self, ErrorCode::InvalidUnicodeCodePoint);
-                }
-            }
-        }
+        self.parse_str_bytes(scratch, true, as_str)
+    }
+
+    fn parse_str_raw<'s>(
+        &'s mut self,
+        scratch: &'s mut Vec<u8>
+    ) -> Result<&'s [u8]> {
+        self.parse_str_bytes(scratch, false, |_, bytes| Ok(bytes))
     }
 }
 
@@ -215,6 +253,7 @@ impl<'a> SliceRead<'a> {
     fn parse_str_bytes<'s, T, F>(
         &'s mut self,
         scratch: &'s mut Vec<u8>,
+        validate: bool,
         result: F
     ) -> Result<T>
         where T: 's,
@@ -252,7 +291,10 @@ impl<'a> SliceRead<'a> {
                     start = self.index;
                 }
                 _ => {
-                    return error(self, ErrorCode::InvalidUnicodeCodePoint);
+                    if validate {
+                        return error(self, ErrorCode::InvalidUnicodeCodePoint);
+                    }
+                    self.index += 1;
                 }
             }
         }
@@ -305,8 +347,16 @@ impl<'a> Read for SliceRead<'a> {
         &'s mut self,
         scratch: &'s mut Vec<u8>
     ) -> Result<&'s str> {
-        self.parse_str_bytes(scratch, as_str)
+        self.parse_str_bytes(scratch, true, as_str)
     }
+
+    fn parse_str_raw<'s>(
+        &'s mut self,
+        scratch: &'s mut Vec<u8>
+    ) -> Result<&'s [u8]> {
+        self.parse_str_bytes(scratch, false, |_, bytes| Ok(bytes))
+    }
+
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -349,11 +399,18 @@ impl<'a> Read for StrRead<'a> {
         &'s mut self,
         scratch: &'s mut Vec<u8>
     ) -> Result<&'s str> {
-        self.delegate.parse_str_bytes(scratch, |_, bytes| {
+        self.delegate.parse_str_bytes(scratch, true, |_, bytes| {
             // The input is assumed to be valid UTF-8 and the \u-escapes are
             // checked along the way, so don't need to check here.
             Ok(unsafe { str::from_utf8_unchecked(bytes) })
         })
+    }
+
+    fn parse_str_raw<'s>(
+        &'s mut self,
+        scratch: &'s mut Vec<u8>
+    ) -> Result<&'s [u8]> {
+        self.delegate.parse_str_raw(scratch)
     }
 }
 

commit 9bd9d3f7ba14fbcf7f84b4f9d947b99be8d394cb

Fix performance regression caused by 8123ecc474ea599d4659f20c3cfbfb7011b77628

json/src/de.rs
@@ -699,12 +699,11 @@ impl<'a, R: Read> de::Deserializer for &'a mut Deserializer<R> {
     fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value>
         where V: de::Visitor
     {
-        try!(self.parse_whitespace());
+        if try!(self.parse_whitespace()) { // true if eof
+            return Err(self.peek_error(ErrorCode::EOFWhileParsingValue));
+        }
 
-        let next = try!(self.peek());
-        let next = try!(next.ok_or(self.peek_error(ErrorCode::ExpectedSomeString)));
-
-        match next {
+        match try!(self.peek_or_null()) {
             b'"' => {
                 self.eat_char();
                 let slice = try!(self.read.parse_str_raw(&mut self.str_buf));