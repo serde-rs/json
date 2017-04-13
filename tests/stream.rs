0d3fd8b0be35df7d62dc6db532b18bdf101c86dd
David Tolnay <dtolnay@gmail.com>
2017-04-13 09:18:49 -0700

    Run the existing tests against every type of stream

--- tests/stream.rs
+++ tests/stream.rs
@@ -5,53 +5,84 @@ extern crate serde_json;
 
 use serde_json::{Deserializer, Map, Value};
 
+macro_rules! test_stream {
+    ($data:expr, $ty:ty, |$stream:ident| $test:block) => {
+        {
+            let de = Deserializer::from_str($data);
+            let mut $stream = de.into_iter::<$ty>();
+            $test
+        }
+        {
+            let de = Deserializer::from_slice($data.as_bytes());
+            let mut $stream = de.into_iter::<$ty>();
+            $test
+        }
+        {
+            let de = Deserializer::from_iter($data.bytes().map(Ok));
+            let mut $stream = de.into_iter::<$ty>();
+            $test
+        }
+        {
+            let mut bytes = $data.as_bytes();
+            let de = Deserializer::from_reader(&mut bytes);
+            let mut $stream = de.into_iter::<$ty>();
+            $test
+        }
+    }
+}
+
 #[test]
 fn test_json_stream_newlines() {
     let data = "{\"x\":39} {\"x\":40}{\"x\":41}\n{\"x\":42}";
-    let mut parsed = Deserializer::from_str(data).into_iter::<Value>();
 
-    assert_eq!(parsed.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 39);
-    assert_eq!(parsed.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 40);
-    assert_eq!(parsed.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 41);
-    assert_eq!(parsed.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 42);
-    assert!(parsed.next().is_none());
+    test_stream!(data, Value, |stream| {
+        assert_eq!(stream.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 39);
+        assert_eq!(stream.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 40);
+        assert_eq!(stream.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 41);
+        assert_eq!(stream.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 42);
+        assert!(stream.next().is_none());
+    });
 }
 
 #[test]
 fn test_json_stream_trailing_whitespaces() {
     let data = "{\"x\":42} \t\n";
-    let mut parsed = Deserializer::from_str(data).into_iter::<Value>();
 
-    assert_eq!(parsed.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 42);
-    assert!(parsed.next().is_none());
+    test_stream!(data, Value, |stream| {
+        assert_eq!(stream.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 42);
+        assert!(stream.next().is_none());
+    });
 }
 
 #[test]
 fn test_json_stream_truncated() {
     let data = "{\"x\":40}\n{\"x\":";
-    let mut parsed = Deserializer::from_str(data).into_iter::<Value>();
 
-    assert_eq!(parsed.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 40);
-    assert!(parsed.next().unwrap().is_err());
-    assert!(parsed.next().is_none());
+    test_stream!(data, Value, |stream| {
+        assert_eq!(stream.next().unwrap().ok().unwrap().pointer("/x").unwrap(), 40);
+        assert!(stream.next().unwrap().is_err());
+        assert!(stream.next().is_none());
+    });
 }
 
 #[test]
 fn test_json_stream_empty() {
     let data = "";
-    let mut parsed = Deserializer::from_str(data).into_iter::<Value>();
 
-    assert!(parsed.next().is_none());
+    test_stream!(data, Value, |stream| {
+        assert!(stream.next().is_none());
+    });
 }
 
 #[test]
 fn test_json_stream_primitive() {
     let data = "{} true";
-    let mut parsed = Deserializer::from_str(data).into_iter::<Value>();
 
-    let first = parsed.next().unwrap().unwrap();
-    assert_eq!(first, Value::Object(Map::new()));
+    test_stream!(data, Value, |stream| {
+        let first = stream.next().unwrap().unwrap();
+        assert_eq!(first, Value::Object(Map::new()));
 
-    let second = parsed.next().unwrap().unwrap_err();
-    assert_eq!(second.to_string(), "expected `{` or `[` at line 1 column 4");
+        let second = stream.next().unwrap().unwrap_err();
+        assert_eq!(second.to_string(), "expected `{` or `[` at line 1 column 4");
+    });
 }
