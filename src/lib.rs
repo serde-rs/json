bea035b19d21bdfe709071be07aea17d5b4bb031
David Tolnay <dtolnay@gmail.com>
2017-10-15 10:40:24 -0700

    Release 1.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -309,7 +309,7 @@
 //! [to_writer]: https://docs.serde.rs/serde_json/ser/fn.to_writer.html
 //! [macro]: https://docs.serde.rs/serde_json/macro.json.html
 
-#![doc(html_root_url = "https://docs.rs/serde_json/1.0.3")]
+#![doc(html_root_url = "https://docs.rs/serde_json/1.0.4")]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Because of "JavaScript"... fixed in Manishearth/rust-clippy#1071
 #![cfg_attr(feature = "cargo-clippy", allow(doc_markdown))]
