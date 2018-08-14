bf352eb96fc6bf9d771d4c955e0ef1306e79be74
David Tolnay <dtolnay@gmail.com>
2018-08-14 09:06:48 -0700

    Release 1.0.25

--- src/lib.rs
+++ src/lib.rs
@@ -315,7 +315,7 @@
 //! [macro]: https://docs.serde.rs/serde_json/macro.json.html
 //! [`serde-json-core`]: https://japaric.github.io/serde-json-core/serde_json_core/
 
-#![doc(html_root_url = "https://docs.rs/serde_json/1.0.24")]
+#![doc(html_root_url = "https://docs.rs/serde_json/1.0.25")]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 // Whitelisted clippy lints
 #![cfg_attr(
