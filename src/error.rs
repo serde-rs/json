6468692902a4763435696d0c39852ee11a2b1343
David Tolnay <dtolnay@gmail.com>
2019-01-23 01:14:13 -0800

    Work around erased-serde round-tripping through de::Error::custom

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@
 use std::error;
 use std::fmt::{self, Debug, Display};
 use std::io;
+use std::str::FromStr;
 use std::result;
 
 use serde::de;
@@ -388,13 +389,7 @@ impl Debug for Error {
 impl de::Error for Error {
     #[cold]
     fn custom<T: Display>(msg: T) -> Error {
-        Error {
-            err: Box::new(ErrorImpl {
-                code: ErrorCode::Message(msg.to_string().into_boxed_str()),
-                line: 0,
-                column: 0,
-            }),
-        }
+        make_error(msg.to_string())
     }
 
     #[cold]
@@ -410,12 +405,68 @@ impl de::Error for Error {
 impl ser::Error for Error {
     #[cold]
     fn custom<T: Display>(msg: T) -> Error {
-        Error {
-            err: Box::new(ErrorImpl {
-                code: ErrorCode::Message(msg.to_string().into_boxed_str()),
-                line: 0,
-                column: 0,
-            }),
-        }
+        make_error(msg.to_string())
+    }
+}
+
+// Parse our own error message that looks like "{} at line {} column {}" to work
+// around erased-serde round-tripping the error through de::Error::custom.
+fn make_error(mut msg: String) -> Error {
+    let (line, column) = parse_line_col(&mut msg).unwrap_or((0, 0));
+    Error {
+        err: Box::new(ErrorImpl {
+            code: ErrorCode::Message(msg.into_boxed_str()),
+            line: line,
+            column: column,
+        }),
+    }
+}
+
+fn parse_line_col(msg: &mut String) -> Option<(usize, usize)> {
+    let start_of_suffix = match msg.rfind(" at line ") {
+        Some(index) => index,
+        None => return None,
+    };
+
+    // Find start and end of line number.
+    let start_of_line = start_of_suffix + " at line ".len();
+    let mut end_of_line = start_of_line;
+    while starts_with_digit(&msg[end_of_line..]) {
+        end_of_line += 1;
+    }
+
+    if !msg[end_of_line..].starts_with(" column ") {
+        return None;
+    }
+
+    // Find start and end of column number.
+    let start_of_column = end_of_line + " column ".len();
+    let mut end_of_column = start_of_column;
+    while starts_with_digit(&msg[end_of_column..]) {
+        end_of_column += 1;
+    }
+
+    if end_of_column < msg.len() {
+        return None;
+    }
+
+    // Parse numbers.
+    let line = match usize::from_str(&msg[start_of_line..end_of_line]) {
+        Ok(line) => line,
+        Err(_) => return None,
+    };
+    let column = match usize::from_str(&msg[start_of_column..end_of_column]) {
+        Ok(column) => column,
+        Err(_) => return None,
+    };
+
+    msg.truncate(start_of_suffix);
+    Some((line, column))
+}
+
+fn starts_with_digit(slice: &str) -> bool {
+    match slice.as_bytes().get(0) {
+        None => false,
+        Some(&byte) => byte >= b'0' && byte <= b'9',
     }
 }
