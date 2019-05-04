8d7f731c2d70b1d2f03e42c1dc34d2ae778676e8
David Tolnay <dtolnay@gmail.com>
2019-05-04 14:52:16 -0700

    Switch ui tests to trybuild

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,19 +1,5 @@
-extern crate compiletest_rs as compiletest;
-
 #[test]
 fn ui() {
-    compiletest::run_tests(&compiletest::Config {
-        mode: compiletest::common::Mode::Ui,
-        src_base: std::path::PathBuf::from("tests/ui"),
-        target_rustcflags: Some(String::from(
-            "\
-             --edition=2018 \
-             -L tests/deps/target/debug/deps \
-             -Z unstable-options \
-             --extern serde_json \
-             ",
-        )),
-        build_base: std::path::PathBuf::from("target/ui"),
-        ..Default::default()
-    });
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/*.rs");
 }
