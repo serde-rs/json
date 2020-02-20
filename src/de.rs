56c569b0f859e1ae0cfcf73b389cee7b7436a044
David Tolnay <dtolnay@gmail.com>
2020-02-19 20:43:52 -0800

    Suppress buggy excessive_precision lint
    https://github.com/rust-lang/rust-clippy/issues/5201

--- src/de.rs
+++ src/de.rs
@@ -1022,6 +1022,8 @@ impl FromStr for Number {
     }
 }
 
+// Clippy bug: https://github.com/rust-lang/rust-clippy/issues/5201
+#[allow(clippy::excessive_precision)]
 static POW10: [f64; 309] = [
     1e000, 1e001, 1e002, 1e003, 1e004, 1e005, 1e006, 1e007, 1e008, 1e009, //
     1e010, 1e011, 1e012, 1e013, 1e014, 1e015, 1e016, 1e017, 1e018, 1e019, //
