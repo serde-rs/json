6e00729f1f610eac1087678d80613f6979da08ba
David Tolnay <dtolnay@gmail.com>
2020-06-07 00:44:22 -0700

    Fix target arch detection for limb width
    cfg(target_arch) refers to the target of the current code being
    compiled. In the case of the build script, that's the host architecture.
    Cargo provides a separate env variable to build scripts to determine the
    target arch of the library as opposed to the target arch of the build
    script.

--- build.rs
+++ build.rs
@@ -1,15 +1,15 @@
+use std::env;
+
 fn main() {
     // Decide ideal limb width for arithmetic in the float parser. Refer to
     // src/lexical/math.rs for where this has an effect.
-    let limb_width_64 = cfg!(any(
-        target_arch = "aarch64",
-        target_arch = "mips64",
-        target_arch = "powerpc64",
-        target_arch = "x86_64"
-    ));
-    if limb_width_64 {
-        println!("cargo:rustc-cfg=limb_width_64");
-    } else {
-        println!("cargo:rustc-cfg=limb_width_32");
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    match target_arch.as_str() {
+        "aarch64" | "mips64" | "powerpc64" | "x86_64" => {
+            println!("cargo:rustc-cfg=limb_width_64");
+        }
+        _ => {
+            println!("cargo:rustc-cfg=limb_width_32");
+        }
     }
 }
