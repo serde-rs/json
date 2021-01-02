44d7fe33fcd1adfb952384367840d706b7278d9e
David Tolnay <dtolnay@gmail.com>
2021-01-02 13:12:08 -0800

    Intra-doc link for the crate::value link

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -106,7 +106,7 @@ pub use crate::raw::{to_raw_value, RawValue};
 
 /// Represents any valid JSON value.
 ///
-/// See the [`serde_json::value` module documentation](./index.html) for usage examples.
+/// See the [`serde_json::value` module documentation](self) for usage examples.
 #[derive(Clone, Eq, PartialEq)]
 pub enum Value {
     /// Represents a JSON null value.
