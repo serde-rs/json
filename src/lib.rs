379412b13875cfb2e440937c4508944d48ee6c96
David Tolnay <dtolnay@gmail.com>
2021-11-17 12:41:32 -0800

    Release 1.0.71

--- src/lib.rs
+++ src/lib.rs
@@ -300,7 +300,7 @@
 //! [macro]: https://docs.serde.rs/serde_json/macro.json.html
 //! [`serde-json-core`]: https://github.com/rust-embedded-community/serde-json-core
 
-#![doc(html_root_url = "https://docs.rs/serde_json/1.0.70")]
+#![doc(html_root_url = "https://docs.rs/serde_json/1.0.71")]
 // Ignored clippy lints
 #![allow(
     clippy::comparison_chain,
