33c3134cd2b96a955c16335131161d7889e98dd6
David Tolnay <dtolnay@gmail.com>
2021-11-23 10:41:18 -0800

    Elaborate on StrRead::parse_str safety comment

--- src/read.rs
+++ src/read.rs
@@ -654,8 +654,9 @@ impl<'a> Read<'a> for StrRead<'a> {
 
     fn parse_str<'s>(&'s mut self, scratch: &'s mut Vec<u8>) -> Result<Reference<'a, 's, str>> {
         self.delegate.parse_str_bytes(scratch, true, |_, bytes| {
-            // The input is assumed to be valid UTF-8 and the \u-escapes are
-            // checked along the way, so don't need to check here.
+            // The deserialization input came in as &str with a UTF-8 guarantee,
+            // and the \u-escapes are checked along the way, so don't need to
+            // check here.
             Ok(unsafe { str::from_utf8_unchecked(bytes) })
         })
     }
