18a88dad66e25d3cda4aa16d64a716e82ac3989d
David Tolnay <dtolnay@gmail.com>
2021-12-30 21:43:05 -0800

    Avoid evaluating $c more than once in overflow macro

--- src/de.rs
+++ src/de.rs
@@ -93,7 +93,9 @@ impl<'a> Deserializer<read::StrRead<'a>> {
 
 macro_rules! overflow {
     ($a:ident * 10 + $b:ident, $c:expr) => {
-        $a >= $c / 10 && ($a > $c / 10 || $b > $c % 10)
+        match $c {
+            c => $a >= c / 10 && ($a > c / 10 || $b > c % 10),
+        }
     };
 }
 
