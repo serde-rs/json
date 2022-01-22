e5cdfcc7ee482238a654afbf3c0116a9f852ce79
David Tolnay <dtolnay@gmail.com>
2022-01-22 15:08:36 -0800

    Support deserializing map key as &RawValue

--- src/de.rs
+++ src/de.rs
@@ -2184,10 +2184,18 @@ where
     }
 
     #[inline]
-    fn deserialize_newtype_struct<V>(self, _name: &'static str, visitor: V) -> Result<V::Value>
+    fn deserialize_newtype_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value>
     where
         V: de::Visitor<'de>,
     {
+        #[cfg(feature = "raw_value")]
+        {
+            if name == crate::raw::TOKEN {
+                return self.de.deserialize_raw_value(visitor);
+            }
+        }
+
+        let _ = name;
         visitor.visit_newtype_struct(self)
     }
 
