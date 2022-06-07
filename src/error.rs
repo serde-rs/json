de5c34b704c7a6cb764c5ef5742457c7f00155e3
David Tolnay <dtolnay@gmail.com>
2022-06-07 00:02:25 -0700

    Resolve get_first clippy lint
        error: accessing first element with `slice.as_bytes().get(0)`
           --> src/error.rs:441:11
            |
        441 |     match slice.as_bytes().get(0) {
            |           ^^^^^^^^^^^^^^^^^^^^^^^ help: try: `slice.as_bytes().first()`
            |
            = note: `-D clippy::get-first` implied by `-D clippy::all`
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#get_first

--- src/error.rs
+++ src/error.rs
@@ -438,7 +438,7 @@ fn parse_line_col(msg: &mut String) -> Option<(usize, usize)> {
 }
 
 fn starts_with_digit(slice: &str) -> bool {
-    match slice.as_bytes().get(0) {
+    match slice.as_bytes().first() {
         None => false,
         Some(&byte) => byte >= b'0' && byte <= b'9',
     }
