b87778bf54dd4d7dc63aacd4c1d7f48e33317fa2
Kevin Velasco <me@kvnvelasco.com>
2022-06-24 11:20:05 +0800

    don't use experimental (in 1.36) Self:: access for enums

--- src/value/from.rs
+++ src/value/from.rs
@@ -275,7 +275,7 @@ where
 {
     fn from(opt: Option<T>) -> Self {
         match opt {
-            None => Self::Null,
+            None => Value::Null,
             Some(value) => Into::into(value),
         }
     }
