c9bff92c1fc750bc6e71af15ecb9498cb75b1cf5
David Tolnay <dtolnay@gmail.com>
2023-03-27 09:47:21 -0700

    Fix PartialEq between Value and f32
    Caught by test_partialeq_number:
    
        thread 'test_partialeq_number' panicked at 'assertion failed: `(left == right)`
          left: `-3.4028235e38`,
         right: `Number(-3.4028235e38)`', tests/test.rs:2033:5

--- src/number.rs
+++ src/number.rs
@@ -279,6 +279,17 @@ impl Number {
         }
     }
 
+    pub(crate) fn as_f32(&self) -> Option<f32> {
+        #[cfg(not(feature = "arbitrary_precision"))]
+        match self.n {
+            N::PosInt(n) => Some(n as f32),
+            N::NegInt(n) => Some(n as f32),
+            N::Float(n) => Some(n as f32),
+        }
+        #[cfg(feature = "arbitrary_precision")]
+        self.n.parse::<f32>().ok().filter(|float| float.is_finite())
+    }
+
     pub(crate) fn from_f32(f: f32) -> Option<Number> {
         if f.is_finite() {
             let n = {
