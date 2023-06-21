b0fa9788f4f1fc03a9fc8066ea4c2aa0f26c5400
Lucien Greathouse <me@lpghatguy.com>
2023-06-21 18:24:47 -0400

    Change MapKeySerializer::serialize_some to fall through instead of erroring

--- src/ser.rs
+++ src/ser.rs
@@ -1043,11 +1043,12 @@ where
         Err(key_must_be_a_string())
     }
 
-    fn serialize_some<T>(self, _value: &T) -> Result<()>
+    #[inline]
+    fn serialize_some<T>(self, value: &T) -> Result<()>
     where
         T: ?Sized + Serialize,
     {
-        Err(key_must_be_a_string())
+        value.serialize(self)
     }
 
     fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
