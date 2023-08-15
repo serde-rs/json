0daacdd52e4abb4a47a7f7fc598c5bd4492aa104
David Tolnay <dtolnay@gmail.com>
2023-08-15 15:05:28 -0700

    Release 1.0.105

--- src/lib.rs
+++ src/lib.rs
@@ -300,7 +300,7 @@
 //! [macro]: crate::json
 //! [`serde-json-core`]: https://github.com/rust-embedded-community/serde-json-core
 
-#![doc(html_root_url = "https://docs.rs/serde_json/1.0.104")]
+#![doc(html_root_url = "https://docs.rs/serde_json/1.0.105")]
 // Ignored clippy lints
 #![allow(
     clippy::collapsible_else_if,
