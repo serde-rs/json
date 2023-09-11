04f7758b6eae935237574b25a1e63cf5e281e19e
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2023-09-11 11:23:47 +0200

    fixup! chore: Remove no_btreemap_get_key_value and no_btreemap_remove_entry.

--- build.rs
+++ build.rs
@@ -1,6 +1,4 @@
 use std::env;
-use std::process::Command;
-use std::str::{self, FromStr};
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
@@ -16,21 +14,4 @@ fn main() {
             println!("cargo:rustc-cfg=limb_width_32");
         }
     }
-
-    let minor = match rustc_minor_version() {
-        Some(minor) => minor,
-        None => return,
-    };
-}
-
-fn rustc_minor_version() -> Option<u32> {
-    let rustc = env::var_os("RUSTC")?;
-    let output = Command::new(rustc).arg("--version").output().ok()?;
-    let version = str::from_utf8(&output.stdout).ok()?;
-    let mut pieces = version.split('.');
-    if pieces.next() != Some("rustc 1") {
-        return None;
-    }
-    let next = pieces.next()?;
-    u32::from_str(next).ok()
 }
