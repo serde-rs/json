b9bcbad3c094042f79f1a45189ec0edbb8f9f322
David Tolnay <dtolnay@gmail.com>
2023-12-28 15:38:25 -0800

    Use BuildHasher::hash_one

--- tests/test.rs
+++ tests/test.rs
@@ -34,7 +34,9 @@ use std::collections::BTreeMap;
 #[cfg(feature = "raw_value")]
 use std::collections::HashMap;
 use std::fmt::{self, Debug};
-use std::hash::{BuildHasher, Hash, Hasher};
+use std::hash::BuildHasher;
+#[cfg(feature = "raw_value")]
+use std::hash::{Hash, Hasher};
 use std::io;
 use std::iter;
 use std::marker::PhantomData;
@@ -2490,11 +2492,7 @@ fn test_value_into_deserializer() {
 #[test]
 fn hash_positive_and_negative_zero() {
     let rand = std::hash::RandomState::new();
-    let hash = |obj: Number| -> u64 {
-        let mut hasher = rand.build_hasher();
-        obj.hash(&mut hasher);
-        hasher.finish()
-    };
+    let hash = |obj| rand.hash_one(obj);
 
     let k1 = serde_json::from_str::<Number>("0.0").unwrap();
     let k2 = serde_json::from_str::<Number>("-0.0").unwrap();
