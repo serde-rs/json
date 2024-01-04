c80dbaf8ff5902ca670e0a48bbe495065b9314f5
WANG Rui <wangrui@loongson.cn>
2024-01-04 14:53:35 +0800

    Set limb width to 64 for loongarch64

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
     // src/lexical/math.rs for where this has an effect.
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     match target_arch.as_str() {
-        "aarch64" | "mips64" | "powerpc64" | "x86_64" => {
+        "aarch64" | "mips64" | "powerpc64" | "x86_64" | "loongarch64" => {
             println!("cargo:rustc-cfg=limb_width_64");
         }
         _ => {
